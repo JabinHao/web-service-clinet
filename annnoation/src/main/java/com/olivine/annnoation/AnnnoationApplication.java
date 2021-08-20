7185ee87c6b31121483deca34e30c84507749120
Olivine <haojipeng@bilibili.com>
2021-08-20 11:47:07 +0800

    project init

---
+++ annnoation/src/main/java/com/olivine/annnoation/AnnnoationApplication.java
@@ -0,0 +1,13 @@
+package com.olivine.annnoation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnnoationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnnoationApplication.class, args);
+    }
+
+}
---
+++ annnoation/src/test/java/com/olivine/annnoation/AnnnoationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivine.annnoation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnnoationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
